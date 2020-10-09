c7a64f4b5c6230565971028bd87c8a40ebeefbf3
ramanmehta7 <ramanmehta833@gmail.com>
2020-10-09 16:58:59 +0530

    banner base

---
+++ header.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <!--    BootStrap-->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"/>
+
+    <!--    MDB-->
+    <!--    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">-->
+    <!--    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap">-->
+    <!--    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css" rel="stylesheet">-->
+    <!--    <link href="https://cdnjs.cloudflare.com/ajax/libs/mdbootstrap/4.19.1/css/mdb.min.css" rel="stylesheet">-->
+
+    <!--    Fonts-->
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Chilanka&display=swap" rel="stylesheet">
+
+    <!--    Customized-->
+    <link rel="stylesheet" href="css/common.css">
+</head>
+</html>
+
+<!--git push https://ramanmehta7:PASSWORD@github.com/ramanmehta7/patella.git-->
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php include("header.php"); ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Prachi Patel</title>
+</head>
+
+<body>
+<section class="banner">
+    <div class="banner-container">
+        <img src="resources/images/splash.png"/>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Morbi tincidunt ornare massa eget egestas purus. Tortor at risus viverra adipiscing
+            at in. Purus faucibus ornare suspendisse sed nisi lacus sed. Nulla facilisi etiam dignissim diam.
+            Bibendum at varius vel pharetra vel. Id nibh tortor id aliquet lectus proin. Quis risus sed vulputate
+            odio ut enim blandit volutpat.
+        </p>
+        <div class="left-banner">
+            <h1>Prachi Patel</h1>
+        </div>
+    </div>
+</section>
+</body>
+
+<?php include("footer.php"); ?>
+</html>
